30a46a3a04b84bc018f3d8293179cbf82bbd0138
Pedro Alfonzo Roque <117417321+pmtalfonzoroque@users.noreply.github.com>
2022-11-23 23:44:22 -0600

    Add files via upload

---
+++ QuickAccountantAppIdentity.Web/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿var myModal;
+
+function showModal(modalId) {
+    myModal = new bootstrap.Modal(document.getElementById(modalId), {});
+    myModal.show();
+}
+
+function hideModal(modalId) {
+    myModal.hide();
+}
